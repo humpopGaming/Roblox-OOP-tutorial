4ae92c7ecbd2e9f0a0b5c7b8840c81453cf9f341
humpopGaming <humpop.gaming@gmail.com>
2024-10-16 21:26:20 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,17 +1,15 @@
-local F8 = require(script.Parent.Vehicle.Car.Ferrari.F8)
-local Porsche911 = require(script.Parent.Vehicle.Car.Porsche.Porsche911)
+local ServerScriptService = game:GetService("ServerScriptService")
+local Ferrari = require(ServerScriptService.OOP.Car.Ferrari)
+local Porsche = require(ServerScriptService.OOP.Car.Porsche)
 
-local f8 = F8.new()
-local porsche911 = Porsche911.new()
+local ferrari = Ferrari.new()
+local porsche = Porsche.new()
 
-f8:Start()
-f8:Accelerate(120)
-f8:Stop()
+ferrari:Drive()
+ferrari:Brake()
 
-porsche911:Start()
-porsche911:Accelerate(160)
-porsche911:Stop()
+porsche:Drive()
+porsche:Brake()
 
-print(f8.Speed)
-print(porsche911.Speed)
-print(porsche911:CheckTyrePressure())
+ferrari:ActivateLaunchControl()
+porsche:ActivateSportChrono()
